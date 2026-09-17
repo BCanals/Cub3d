4c67454c223e2b2f3b5a79184d42c03ba28c5479
Laia Artes Sebastia <lartes-s@car9s2.42barcelona.com>
2026-09-17 20:00:45 +0200

    holiwi

--- inc/cub.h
+++ inc/cub.h
@@ -6,7 +6,7 @@
 /*   By: lartes-s <lartes-s@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/17 18:56:40 by lartes-s          #+#    #+#             */
-/*   Updated: 2026/09/17 19:13:55 by lartes-s         ###   ########.fr       */
+/*   Updated: 2026/09/17 19:59:57 by lartes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,40 @@
 # include <math.h>
 # include <errno.h>
 
+typedef struct s_player
+{
+	float	pos_x;
+	float	pos_y;
+	float	dir_x;
+	float	dir_y;
+}				t_player;
 
+typedef struct	s_color
+{
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+}		t_color;
+
+typedef struct	s_map
+{
+	char	*tex_n;
+	char	*tex_s;
+	char	*tex_e;
+	char	*tex_o;
+	int		n_cols;
+	int		n_rows;
+	char	**map; 
+	t_color	floor;
+	t_color	sky;
+}				t_map;
+
+typedef	struct	s_game
+{
+	t_player	player;
+	t_map		map;
+	t_mlx		*mlx; // ho omple la Laia
+	
+}				t_game;
 
 #endif
-
